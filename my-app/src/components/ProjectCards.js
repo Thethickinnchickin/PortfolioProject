cc3661fcab79a3df4c8e2b64fdfc2b37aa7f2ab4
Matt <matt.reiley@sjsu.edu>
2022-10-19 00:16:43 -0700

    sudoku solver

--- my-app/src/components/ProjectCards.js
+++ my-app/src/components/ProjectCards.js
@@ -5,41 +5,50 @@ import './ProjectCards.css'
 function ProjectCards() {
   return(
       <div className='cards'>
-        <h1>Check out these Projects</h1>
+        <h1>Check out my Projects</h1>
         <div className='cards__container'>
             <div className='cards_wrapper'>
                 <ul className='cards__items'>
                     <CardItem 
-                        src='images/friendslink.png'
-                        text='Social Platfrom made with ASP Net Core and Angular'
-                        label='Friends-Link'
-                        path='https://friendslink.herokuapp.com/'
-                    />
-                    <CardItem 
-                        src='images/workout.png'
-                        text='Workout App made with Nuxtjs'
-                        path='https://calm-brushlands-65146.herokuapp.com/'
-                        label='League-Workout'
-                    />
-                    <CardItem 
-                        src='images/coffee-calling.png'
-                        text='E-Commerce-Site Made with Nuxt and Express'
-                        label='Coffee-Calling'
-                        path='https://coffee-e-commerce.herokuapp.com/'
-                    />
-                </ul>
-                <ul className='cards__items'>
-                <CardItem 
                         src='images/calc.png'
                         text='Calculator made with javascriipt'
                         label='Graping Calculator'
                         path='https://reiley-calculatorr.herokuapp.com/'
                     />
                     <CardItem 
+                        src='images/friendslink.png'
+                        text='Social Platfrom made with ASP Net Core and Angular'
+                        label='Friends-Link'
+                        path='https://friendslink.herokuapp.com/'
+                    />
+
+                    <CardItem 
+                        src='images/coffee-calling.png'
+                        text='E-Commerce-Site Made with Nuxt and Express'
+                        label='Coffee-Calling'
+                        path='https://coffee-e-commerce.herokuapp.com/'
+                    />
+                    <CardItem 
+                        src='images/sudoku.png'
+                        text='Sudoku Solver made with python'
+                        label='Sudoku Solver'
+                        path='https://reiley-calculatorr.herokuapp.com/'
+                    />
+                </ul>
+                <ul className='cards__items'>
+
+                    <CardItem 
                         src='images/restaurantreview.jpg'
                         text='Restaurant Review with Express and EJS'
                         label='Restaurant Review'
                         path='https://rocky-meadow-39048.herokuapp.com'
+                    />
+
+                    <CardItem 
+                        src='images/workout.png'
+                        text='Workout App made with Nuxtjs'
+                        path='https://calm-brushlands-65146.herokuapp.com/'
+                        label='League-Workout'
                     />
 
                     <CardItem 
@@ -49,6 +58,9 @@ function ProjectCards() {
                         path='https://github.com/Thethickinnchickin'
                         label='GitHub'
                     />
+                </ul>
+                <ul className='cards__items'>
+
                 </ul>
             </div>
             
